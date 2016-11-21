97bd29abf199e4938bcc7972d4ddebc1a19bd609
Mark Watson <mark.watson.x@gmail.com>
2016-11-21 09:28:03 -0600

    Graphs support for ingredients, cuisines, recipes, and favorites

--- souschef/recipe_graph.py
+++ souschef/recipe_graph.py
@@ -1,5 +1,7 @@
 from graph_db_ops import GraphDbOps
 
+import json
+
 
 class RecipeGraph(object):
     def __init__(self, credentials, logger):
@@ -37,6 +39,8 @@ class RecipeGraph(object):
         else:
             self.logger.info("Graph Schema exists.")
 
+    # User
+
     def add_user_vertex(self, user_id):
         vertex = {
             'label': 'person',
@@ -45,7 +49,135 @@ class RecipeGraph(object):
             }
         }
         return self.add_vertex_if_not_exists(vertex, 'name')
-    
+
+    # Ingredients
+
+    @staticmethod
+    def get_unique_ingredients_name(ingredients_str):
+        ingredients = [x.strip() for x in ingredients_str.lower().strip().split(',')]
+        ingredients.sort()
+        return ','.join([x for x in ingredients])
+
+    def find_ingredients_vertex(self, ingredients_str):
+        return self.find_vertex('ingredient', 'name', self.get_unique_ingredients_name(ingredients_str))
+
+    def add_ingredients_vertex(self, ingredients_str, matching_recipes, user_vertex):
+        ingredient_vertex = {
+            'label': 'ingredient',
+            'properties': {
+                'name': self.get_unique_ingredients_name(ingredients_str),
+                'detail': json.dumps(matching_recipes)
+            }
+        }
+        ingredient_vertex = self.add_vertex_if_not_exists(ingredient_vertex, 'name')
+        self.increment_ingredient_edge(ingredient_vertex, user_vertex)
+        return ingredient_vertex
+
+    def increment_ingredient_edge(self, ingredient_vertex, user_vertex):
+        ingredient_edge = {
+            'label': 'selects',
+            'outV': user_vertex['id'],
+            'inV': ingredient_vertex['id'],
+            'properties': {
+                'count': 1
+            }
+        }
+        self.add_update_edge(ingredient_edge)
+
+    # Cuisine
+
+    @staticmethod
+    def get_unique_cuisine_name(cuisine):
+        return cuisine.strip().lower()
+
+    def find_cuisine_vertex(self, cuisine_str):
+        return self.find_vertex('cuisine', 'name', self.get_unique_cuisine_name(cuisine_str))
+
+    def add_cuisine_vertex(self, cuisine_str, matching_recipes, user_vertex):
+        cuisine_vertex = {
+            'label': 'cuisine',
+            'properties': {
+                'name': self.get_unique_cuisine_name(cuisine_str),
+                'detail': json.dumps(matching_recipes)
+            }
+        }
+        cuisine_vertex = self.add_vertex_if_not_exists(cuisine_vertex, 'name')
+        self.increment_cuisine_edge(cuisine_vertex, user_vertex)
+        return cuisine_vertex
+
+    def increment_cuisine_edge(self, cuisine_vertex, user_vertex):
+        cuisine_edge = {
+            'label': 'selects',
+            'outV': user_vertex['id'],
+            'inV': cuisine_vertex['id'],
+            'properties': {
+                'count': 1
+            }
+        }
+        self.add_update_edge(cuisine_edge)
+
+    # Recipe
+
+    @staticmethod
+    def get_unique_recipe_name(recipe_id):
+        return str(recipe_id).strip().lower()
+
+    def find_recipe_vertex(self, recipe_id):
+        return self.find_vertex('recipe', 'name', self.get_unique_recipe_name(recipe_id))
+
+    def find_recipes_for_user(self, user_id):
+        query = 'g.V().hasLabel("person").has("name", "{}").outE().inV().hasLabel("recipe").path()'.format(user_id)
+        response = self.db_ops.execute_gremlin_query(query)
+        if len(response) > 0:
+            return response
+        else:
+            return None
+
+    def add_recipe_vertex(self, recipe_id, recipe_title, recipe_detail, ingredient_cuisine_vertex, user_vertex):
+        recipe_vertex = {
+            'label': 'recipe',
+            'properties': {
+                'name': self.get_unique_recipe_name(recipe_id),
+                'title': recipe_title.strip(),
+                'detail': recipe_detail
+            }
+        }
+        recipe_vertex = self.add_vertex_if_not_exists(recipe_vertex, 'name')
+        self.increment_recipe_edges(recipe_vertex, ingredient_cuisine_vertex, user_vertex)
+        return recipe_vertex
+
+    def increment_recipe_edges(self, recipe_vertex, ingredient_cuisine_vertex, user_vertex):
+        # add one edge from the user to the recipe (this will let us find a user's favorite recipes, etc)
+        edge = {
+            'label': 'selects',
+            'outV': user_vertex['id'],
+            'inV': recipe_vertex['id'],
+            'properties': {
+                'count': 1
+            }
+        }
+        self.add_update_edge(edge)
+        if ingredient_cuisine_vertex is not None:
+            edge = {
+                'label': 'selects',
+                'outV': ingredient_cuisine_vertex['id'],
+                'inV': recipe_vertex['id'],
+                'properties': {
+                    'count': 1
+                }
+            }
+            self.add_update_edge(edge)
+
+    # Graph Helper Methods
+
+    def find_vertex(self, label, property_name, property_value):
+        query = 'g.V().hasLabel("{}").has("{}", "{}")'.format(label, property_name, property_value)
+        response = self.db_ops.execute_gremlin_query(query)
+        if len(response) > 0:
+            return response[0]
+        else:
+            return None
+
     def add_vertex_if_not_exists(self, vertex, unique_property_name):
         property_value = vertex['properties'][unique_property_name]
         query = 'g.V().hasLabel("{}").has("{}", "{}")'.format(vertex['label'], unique_property_name, property_value)
@@ -56,3 +188,16 @@ class RecipeGraph(object):
         else:
             self.logger.info("Creating {} vertex where {}={}".format(vertex['label'], unique_property_name, property_value))
             return self.db_ops.add_or_update_vertex(vertex)
+
+    def add_update_edge(self, edge):
+        query = 'g.V({}).outE().inV().hasId({}).path()'.format(edge['outV'], edge['inV'])
+        response = self.db_ops.execute_gremlin_query(query)
+        if len(response) > 0:
+            edge = response[0]['objects'][1]
+            count = 0
+            if 'count' in edge['properties'].keys():
+                count = edge['properties']['count']
+            edge['properties']['count'] = count + 1
+            self.db_ops.add_or_update_edge(edge, edge['id'])
+        else:
+            self.db_ops.add_or_update_edge(edge)
--- souschef/souschef.py
+++ souschef/souschef.py
@@ -29,7 +29,7 @@ class SousChef:
         output_list = slack_rtm_output
         if output_list and len(output_list) > 0:
             for output in output_list:
-                print json.dumps(output)
+                # print json.dumps(output)
                 if output and 'text' in output and 'user_profile' not in output and self.at_bot in output['text']:
                     return output['text'].split(self.at_bot)[1].strip().lower(), output['user'], output['channel']
                 elif output and 'text' in output and 'user_profile' not in output:
@@ -77,10 +77,49 @@ class SousChef:
             response += text + "\n"
         return response
 
+    def handle_favorites_message(self, state):
+        matching_recipes = []
+        paths = self.recipe_graph.find_recipes_for_user(state.user_id)
+        if paths is not None and len(paths) > 0:
+            paths.sort(key=lambda x: x['objects'][1]['properties']['count'], reverse=True)
+            for path in paths:
+                matching_recipes.append({
+                    'id': path['objects'][2]['properties']['name'][0]['value'],
+                    'title': path['objects'][2]['properties']['title'][0]['value']
+                })
+        # update state
+        state.conversation_context['recipes'] = matching_recipes
+        state.ingredient_cuisine_vertex = None
+        # return the response
+        response = "Lets see here...\n" + \
+                   "I've found these recipes: \n"
+
+        for i, recipe in enumerate(state.conversation_context['recipes']):
+            response += str(i + 1) + ". " + recipe['title'] + "\n"
+        response += "\nPlease enter the corresponding number of your choice."
+
+        return response
+
     def handle_ingredients_message(self, state, message):
-        if state.conversation_context['get_recipes']:
-            state.conversation_context['recipes'] = \
-                self.recipe_client.find_by_ingredients(message)
+
+        # we want to get a list of recipes based on the ingredients (message)
+        # first we see if we already have the ingredients in our graph
+        ingredients_str = message
+        ingredient_vertex = self.recipe_graph.find_ingredients_vertex(ingredients_str)
+        if ingredient_vertex is not None:
+            print "Ingredients vertex exists for {}. Returning recipes from vertex.".format(ingredients_str)
+            matching_recipes = json.loads(ingredient_vertex['properties']['detail'][0]['value'])
+            # increment the count on the user-ingredient edge
+            self.recipe_graph.increment_ingredient_edge(ingredient_vertex, state.user_vertex)
+        else:
+            # we don't have the ingredients in our graph yet, so get list of recipes from Spoonacular
+            print "Ingredients vertex does not exist for {}. Querying Spoonacular for recipes.".format(ingredients_str)
+            matching_recipes = self.recipe_client.find_by_ingredients(ingredients_str);
+            # add vertex for the ingredients to our graph
+            ingredient_vertex = self.recipe_graph.add_ingredients_vertex(ingredients_str, matching_recipes, state.user_vertex)
+
+        state.conversation_context['recipes'] = matching_recipes
+        state.ingredient_cuisine_vertex = ingredient_vertex
 
         response = "Lets see here...\n" + \
                    "I've found these recipes: \n"
@@ -92,8 +131,24 @@ class SousChef:
         return response
 
     def handle_cuisine_message(self, state, cuisine):
-        if state.conversation_context['get_recipes']:
-            state.conversation_context['recipes'] = self.recipe_client.find_by_cuisine(cuisine)
+        
+        # we want to get a list of recipes based on the cuisine
+        # first we see if we already have the cuisine in our graph
+        cuisine_vertex = self.recipe_graph.find_cuisine_vertex(cuisine)
+        if cuisine_vertex is not None:
+            print "Cuisine vertex exists for {}. Returning recipes from vertex.".format(cuisine)
+            matching_recipes = json.loads(cuisine_vertex['properties']['detail'][0]['value'])
+            # increment the count on the user-cuisine edge
+            self.recipe_graph.increment_cuisine_edge(cuisine_vertex, state.user_vertex)
+        else:
+            # we don't have the cuisine in our graph yet, so get list of recipes from Spoonacular
+            print "Cuisine vertex does not exist for {}. Querying Spoonacular for recipes.".format(cuisine)
+            matching_recipes = self.recipe_client.find_by_cuisine(cuisine);
+            # add vertex for the cuisine to our graph
+            cuisine_vertex = self.recipe_graph.add_cuisine_vertex(cuisine, matching_recipes, state.user_vertex)
+
+        state.conversation_context['recipes'] = matching_recipes
+        state.ingredient_cuisine_vertex = cuisine_vertex
 
         response = "Lets see here...\n" + \
                    "I've found these recipes: \n"
@@ -105,11 +160,32 @@ class SousChef:
         return response
 
     def handle_selection_message(self, state, selection):
-        recipe_id = state.conversation_context['recipes'][selection - 1]['id']
-        recipe_info = self.recipe_client.get_info_by_id(recipe_id)
-        recipe_steps = self.recipe_client.get_steps_by_id(recipe_id)
-
-        return self.make_formatted_steps(recipe_info, recipe_steps)
+        if 1 <= selection <= 5:
+            # we want to get a the recipe based on the selection
+            # first we see if we already have the recipe in our graph
+            recipes = state.conversation_context['recipes']
+            recipe_id = recipes[selection-1]['id']
+            recipe_vertex = self.recipe_graph.find_recipe_vertex(recipe_id)
+            if recipe_vertex is not None:
+                print "Recipe vertex exists for {}. Returning recipe steps from vertex.".format(recipe_id)
+                recipe_detail = recipe_vertex['properties']['detail'][0]['value']
+                # increment the count on the ingredient/cuisine-recipe edge and the user-recipe edge
+                self.recipe_graph.increment_recipe_edges(recipe_vertex, state.ingredient_cuisine_vertex, state.user_vertex)
+            else:
+                print "Recipe vertex does not exist for {}. Querying Spoonacular for details.".format(recipe_id)
+                recipe_info = self.recipe_client.get_info_by_id(recipe_id)
+                recipe_steps = self.recipe_client.get_steps_by_id(recipe_id)
+                recipe_detail = self.make_formatted_steps(recipe_info, recipe_steps);
+                # add vertex for recipe
+                self.recipe_graph.add_recipe_vertex(recipe_id, recipe_info['title'], recipe_detail, state.ingredient_cuisine_vertex, state.user_vertex)
+            # clear out state
+            state.ingredient_cuisine_vertex = None
+            state.conversation_context = None
+            # return response
+            return recipe_detail
+        else:
+            state.conversation_context['selection_valid'] = False
+            return "Invalid selection! Say anything to see your choices again...";
 
     def handle_message(self, message, message_sender, channel):
 
@@ -142,12 +218,9 @@ class SousChef:
 
             if state.conversation_context['selection'].isdigit():
                 selection = int(state.conversation_context['selection'])
-                if selection >= 1 and selection <= 5:
-                    state.conversation_context['selection_valid'] = True
-                    response = self.handle_selection_message(state, selection)
+                response = self.handle_selection_message(state, selection)
 
-        elif watson_response['entities'] and \
-                        watson_response['entities'][0]['entity'] == 'cuisine':
+        elif watson_response['entities'] and watson_response['entities'][0]['entity'] == 'cuisine':
             cuisine = watson_response['entities'][0]['value']
             response = self.handle_cuisine_message(state, cuisine)
 
