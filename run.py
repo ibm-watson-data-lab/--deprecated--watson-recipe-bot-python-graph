b33ec4870cdd50887802771ea4917f966e18a8ca
Mark Watson <mark.watson.x@gmail.com>
2016-12-01 16:55:05 -0600

    Refactor

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 import os
+import sys
+
 from dotenv import load_dotenv
 from ibm_graph import IBMGraphClient
 from slackclient import SlackClient
@@ -9,31 +11,36 @@ from souschef.recipe import RecipeClient
 from souschef.souschef import SousChef
 
 if __name__ == "__main__":
-    load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
+    try:
+        # load environment variables
+        load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
+        slack_bot_id = os.environ.get("SLACK_BOT_ID")
+        slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+        conversation_workspace_id = os.environ.get("CONVERSATION_WORKSPACE_ID")
+        conversation_client = ConversationV1(
+            username=os.environ.get("CONVERSATION_USERNAME"),
+            password=os.environ.get("CONVERSATION_PASSWORD"),
+            version='2016-07-11'
+        )
+        recipe_client = RecipeClient(os.environ.get("SPOONACULAR_KEY"))
+        recipe_store = GraphRecipeStore(
+            IBMGraphClient(
+                os.environ.get("GRAPH_API_URL"),
+                os.environ.get("GRAPH_USERNAME"),
+                os.environ.get("GRAPH_PASSWORD")
+            )
+        )
+        # start the souschef bot
+        souschef = SousChef(slack_bot_id,
+                            slack_client,
+                            conversation_client,
+                            conversation_workspace_id,
+                            recipe_client,
+                            recipe_store)
+        souschef.start()
+        sys.stdin.readline()
+    except (KeyboardInterrupt, SystemExit):
+        pass
+    souschef.stop()
+    souschef.join()
 
-    bot_id = os.environ.get("SLACK_BOT_ID")
-    conversation_workspace_id = os.environ.get("CONVERSATION_WORKSPACE_ID")
-
-    slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
-
-    conversation_client = ConversationV1(
-        username=os.environ.get("CONVERSATION_USERNAME"),
-        password=os.environ.get("CONVERSATION_PASSWORD"),
-        version='2016-07-11'
-    )
-
-    recipe_client = RecipeClient(os.environ.get("SPOONACULAR_KEY"))
-
-    recipe_graph = GraphRecipeStore(IBMGraphClient(
-        os.environ.get("GRAPH_API_URL"),
-        os.environ.get("GRAPH_USERNAME"),
-        os.environ.get("GRAPH_PASSWORD"))
-    )
-
-    souschef = SousChef(recipe_graph,
-                        bot_id,
-                        slack_client,
-                        conversation_client,
-                        conversation_workspace_id,
-                        recipe_client)
-    souschef.run()
